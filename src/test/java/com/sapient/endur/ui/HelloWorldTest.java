51d4f97854ab08792a35d759d786234cbf58658c
SANGATI SIVASANKAR <sansivas@publicisgroupe.net>
2022-04-27 12:28:55 +0530

    first commit

---
+++ src/test/java/com/sapient/endur/ui/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package com.sapient.endur.ui;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HelloWorldTest {
+
+	@Test
+	void test() {
+		assertTrue(true);
+	}
+
+}
